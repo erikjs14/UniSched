4be5b1f7c202d998c18cf6f51c7254b809af11c9
Erik Schake <erik.schake@gmail.com>
2020-05-19 22:17:44 +0200

    Settings card header

--- src/container/settings/subjectSettings/SubjectSettings.tsx
+++ src/container/settings/subjectSettings/SubjectSettings.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 import CSS from './SubjectSettings.module.scss';
 import { SubjectSettingsProps } from './SubjectSettings.d';
@@ -6,23 +8,34 @@ import { toCss } from './../../../util/util';
 import Input from '../../../components/ui/input/Input';
 const {
     wrapper: s_wrapper,
-    settingsCard: s_settingsCard
+    settingsCard: s_settingsCard,
+    header: s_header,
+    trashIcon: s_trashIcon,
 } = CSS;
 
 export default function(props: SubjectSettingsProps): JSX.Element {
 
     const [title, setTitle] = useState('');
+
+    const style = {
+        color: 'white',
+    };
     
     return (
-        <div className={toCss(s_wrapper, 'util-center-content')}>
+        <div style={style} className={toCss(s_wrapper, 'util-center-content')}>
             <div className={toCss(s_settingsCard)}>
 
-                <Input 
-                    elementType='input-transparent'
-                    value={title}
-                    onChange={setTitle}
-                    label='Title'
-                />
+                <div className={toCss(s_header)}>
+                    <Input 
+                        elementType='input-transparent'
+                        value={title}
+                        onChange={setTitle}
+                        label='Title'
+                    />
+                    <div className={toCss(s_trashIcon)}>
+                        <FontAwesomeIcon icon={faTrash} />
+                    </div>
+                </div>
 
             </div>
         </div>
