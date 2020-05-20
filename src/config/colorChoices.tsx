65e9f89d2c32799907fd8c61069a4b0f13db5cf9
Erik Schake <erik.schake@gmail.com>
2020-05-20 16:03:13 +0200

    Color Picker with animation

---
+++ src/config/colorChoices.tsx
@@ -0,0 +1,35 @@
+interface Color {name: string, value: string, textColor: string}
+
+const choices: Color[] = [
+    { name: 'turquoise', value: '#1abc9c', textColor: '#fff' },
+    { name: 'emerald', value: '#2ecc71', textColor: '#fff' },
+    { name: 'greenSea', value: '#16a085', textColor: '#fff' },
+    { name: 'nephritis', value: '#27ae60', textColor: '#fff' },
+    { name: 'peterRiver', value: '#3498db', textColor: '#fff' },
+    { name: 'belizeHole', value: '#2980b9', textColor: '#fff' },
+    { name: 'amethyst', value: '#9b59b6', textColor: '#000' },
+    { name: 'wisteria', value: '#8e44ad', textColor: '#000' },
+    { name: 'wetAsphalt', value: '#34495e', textColor: '#fff' },
+    { name: 'midnightBlue', value: '#2c3e50', textColor: '#fff' },
+    { name: 'sunFlower', value: '#f1c40f', textColor: '#000' },
+    { name: 'orange', value: '#f39c12', textColor: '#000' },
+    { name: 'carrot', value: '#e67e22', textColor: '#000' },
+    { name: 'pumpkin', value: '#d35400', textColor: '#000' },
+    { name: 'alizarin', value: '#e74c3c', textColor: '#000' },
+    { name: 'pomegranate', value: '#c0392b', textColor: '#000' },
+    { name: 'clouds', value: '#ecf0f1', textColor: '#000' },
+    { name: 'silver', value: '#bdc3c7', textColor: '#000' },
+    { name: 'soncrete', value: '#95a5a6', textColor: '#000' },
+    { name: 'asbestos', value: '#7f8c8d', textColor: '#000' },
+];
+
+export default choices;
+
+export const findColorConfig = (name: string): Color => {
+    const val =  choices.find(color => {
+        if (color.name === name) return color;
+        return false;
+    });
+    if (val) return val;
+    else return choices[0];
+}
--- src/container/settings/subjectSettings/SubjectSettings.tsx
+++ src/container/settings/subjectSettings/SubjectSettings.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Transition } from 'react-transition-group';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
@@ -6,38 +7,106 @@ import CSS from './SubjectSettings.module.scss';
 import { SubjectSettingsProps } from './SubjectSettings.d';
 import { toCss } from './../../../util/util';
 import Input from '../../../components/ui/input/Input';
+import colorChoices, { findColorConfig } from '../../../config/colorChoices';
 const {
     wrapper: s_wrapper,
     settingsCard: s_settingsCard,
     header: s_header,
     trashIcon: s_trashIcon,
+    colorRow: s_colorRow,
+    colorPicker: s_colorPicker,
 } = CSS;
 
 export default function(props: SubjectSettingsProps): JSX.Element {
 
     const [title, setTitle] = useState('');
+    const [colorConfig, setColorConfig] = useState({
+        newColor: { name: 'turquoise', value: '#1abc9c', textColor: '#fff' },
+        oldColor: { name: 'turquoise', value: '#1abc9c', textColor: '#fff' },
+    });
+    const [animateColorChange, startAnimateColorChange] = useState(false);
 
-    const style = {
-        color: 'white',
+    const changeColor = (colorName: string): void => {
+        setColorConfig(prev => ({
+            newColor: findColorConfig(colorName),
+            oldColor: prev.newColor,
+        }));
+        startAnimateColorChange(true);
+    }
+
+    const defaultStyle = {
+        transition: 'background-color .3s ease-in-out',
+        backgroundColor: colorConfig.newColor.value,
+        color: colorConfig.newColor.textColor,
     };
+    const colorStyles =  {
+        unmounted: {},
+        entering: { backgroundColor: colorConfig.oldColor.value },
+        entered: { backgroundColor: colorConfig.newColor.value },
+        exiting: {},
+        exited: {},
+    }
     
     return (
-        <div style={style} className={toCss(s_wrapper, 'util-center-content')}>
-            <div className={toCss(s_settingsCard)}>
+        <Transition 
+            in={animateColorChange}
+            timeout={300}
+            exit={false}
+        >
+            {state => {
 
-                <div className={toCss(s_header)}>
-                    <Input 
-                        elementType='input-transparent'
-                        value={title}
-                        onChange={setTitle}
-                        label='Title'
-                    />
-                    <div className={toCss(s_trashIcon)}>
-                        <FontAwesomeIcon icon={faTrash} />
+                return (
+                    <div
+                        className={toCss(s_wrapper, 'util-center-content')}
+                    >
+                        <div
+                            style={{
+                                ...defaultStyle,
+                                ...colorStyles[state],
+                            }} 
+                            className={toCss(s_settingsCard)}
+                        >
+
+                            <div className={toCss(s_header)}>
+                                <Input 
+                                    elementType='input-transparent'
+                                    value={title}
+                                    onChange={setTitle}
+                                    label='Title'
+                                />
+                                <div className={toCss(s_trashIcon)}>
+                                    <FontAwesomeIcon icon={faTrash} />
+                                </div>
+                            </div>
+
+                            <div
+                                className={toCss(s_colorRow)}
+                            >
+                                {colorChoices.map(obj => (
+                                    <label 
+                                        key={obj.name}
+                                        className={toCss(s_colorPicker)}
+                                    >
+                                        <input 
+                                            type='radio'
+                                            value={obj.name}
+                                            onChange={event => changeColor(event.target.value)}
+                                            checked={obj.name === colorConfig.newColor.name}
+                                            name={'color'}
+                                        />
+                                        <span style={{backgroundColor: obj.value}}></span>
+                                        <svg viewBox="0 0 120 120" version="1.1"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <circle cx="60" cy="60" r="50"/>
+                                        </svg>
+                                    </label>
+                                ))}
+                            </div>
+                        </div>
                     </div>
-                </div>
 
-            </div>
-        </div>
+                );
+            }}
+        </Transition>
     );
 }
