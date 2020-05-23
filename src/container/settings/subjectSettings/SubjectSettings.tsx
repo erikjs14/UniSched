c134e3a0d20411b50ac8e9fd1f01736a37206f78
Erik Schake <erik.schake@gmail.com>
2020-05-23 12:24:05 +0200

    Prompt on unsaved changes

--- src/container/settings/subjectSettings/SubjectSettings.tsx
+++ src/container/settings/subjectSettings/SubjectSettings.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useState, useEffect, useCallback } from 'react';
+import React, { useReducer, useState, useEffect, useCallback, Fragment } from 'react';
 import { Transition } from 'react-transition-group';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
@@ -11,7 +11,7 @@ import Input from '../../../components/ui/input/Input';
 import { defaultColor } from '../../../config/colorChoices';
 import ColorPicker from '../../../components/ui/colorPicker/ColorPicker';
 import { fetchSubject, updateSubject, addSubject, deleteSubject } from './../../../firebase/firestore';
-import { useLocation, useHistory } from 'react-router-dom';
+import { useLocation, useHistory, Prompt } from 'react-router-dom';
 import Loader from '../../../components/ui/loader/Loader';
 import { reducer, initialState, setSubject, setError, setLoading, changeName, changeColor, startSaving, setSaved, initialStateNew } from './state';
 const {
@@ -119,117 +119,123 @@ export default React.memo(function(props: SubjectSettingsProps): JSX.Element {
     }
     
     return (
-        <Transition 
-            in={animateColorChange}
-            timeout={300}
-            exit={false}
-        >
-            {transitionState => {
+        <Fragment>
+            <Transition 
+                in={animateColorChange}
+                timeout={300}
+                exit={false}
+            >
+                {transitionState => {
 
-                return (
-                    <div
-                        className={toCss(s_wrapper, 'util-center-content')}
-                    >
+                    return (
                         <div
-                            style={{
-                                ...defaultStyle,
-                                ...colorStyles[transitionState],
-                            }} 
-                            className={toCss(s_settingsCard)}
+                            className={toCss(s_wrapper, 'util-center-content')}
                         >
+                            <div
+                                style={{
+                                    ...defaultStyle,
+                                    ...colorStyles[transitionState],
+                                }} 
+                                className={toCss(s_settingsCard)}
+                            >
 
-                            <div className={toCss(s_header)}>
-                                <Input 
-                                    addClass={s_titleInput}
-                                    elementType='input-transparent'
-                                    value={state.subject?.name || ''}
-                                    onChange={updateTitle}
-                                    label='Title'
-                                    labelColor={state.subject?.color.newColor.textColor}
+                                <div className={toCss(s_header)}>
+                                    <Input 
+                                        addClass={s_titleInput}
+                                        elementType='input-transparent'
+                                        value={state.subject?.name || ''}
+                                        onChange={updateTitle}
+                                        label='Title'
+                                        labelColor={state.subject?.color.newColor.textColor}
+                                    />
+                                    <div 
+                                        className={toCss(s_trashIcon)}
+                                        onClick={() => setWantDelete(true)}    
+                                    >
+                                        <FontAwesomeIcon icon={faTrash} />
+                                    </div>
+                                </div>
+
+                                <ColorPicker
+                                    style={{zIndex: 100}}
+                                    onSelectedColorChanged={updateColor}
+                                    selectedColorName={state.subject?.color.newColor.name || defaultColor()}
                                 />
-                                <div 
-                                    className={toCss(s_trashIcon)}
-                                    onClick={() => setWantDelete(true)}    
-                                >
-                                    <FontAwesomeIcon icon={faTrash} />
+
+                                <div className={toCss(s_settingsArea)}>
+                                
+                                    <div className={toCss(s_eventsArea)}>
+                                        EVENTS
+                                    </div>
+
+                                    <div className={toCss(s_tasksArea)}>
+                                        TASKS
+                                    </div>
+
+                                    <div className={toCss(s_examsArea)}>
+                                        EXAMS
+                                    </div>
+
                                 </div>
+
+                                <div className={toCss(s_footerArea)}>
+                                    <Button 
+                                        appearance='primary' 
+                                        className={toCss(s_saveBtn)}
+                                        iconBefore='floppy-disk'
+                                        isLoading={state.saving}
+                                        disabled={!state.subject?.changed}
+                                        onClick={saveHandler}
+                                    >
+                                        Save
+                                    </Button>
+                                </div>
+                                
                             </div>
 
-                            <ColorPicker
-                                style={{zIndex: 100}}
-                                onSelectedColorChanged={updateColor}
-                                selectedColorName={state.subject?.color.newColor.name || defaultColor()}
-                            />
+                            <Dialog
+                                isShown={wantDelete}
+                                isConfirmLoading={deleting}
+                                title={`Delete subject ${state.subject?.name}`}
+                                confirmLabel='Confirm'
+                                onCancel={close => {
+                                    setWantDelete(false);
+                                    close();
+                                }}
+                                onConfirm={close => {
+                                    if (!state.subject) {
+                                        dispatch(setError('Unexpected error.'));
+                                    } else {
+                                        setDeleting(true);
+                                        deleteSubject(state.subject.id)
+                                            .then(() => {
+                                                close();
+                                                history.push('/settings');
+                                            })
+                                            .catch(error => {
+                                                dispatch(setError(error.message));
+                                                setDeleting(false);
+                                                setWantDelete(false);
+                                            })
+                                    }
+                                }}
+                                onCloseComplete={() => { //closed in another way
+                                    setWantDelete(false);
+                                }}
+                            >
+                                Are you sure you want to delete this subject including all of the exams, events and tasks?
+                            </Dialog>
 
-                            <div className={toCss(s_settingsArea)}>
-                            
-                                <div className={toCss(s_eventsArea)}>
-                                    EVENTS
-                                </div>
-
-                                <div className={toCss(s_tasksArea)}>
-                                    TASKS
-                                </div>
-
-                                <div className={toCss(s_examsArea)}>
-                                    EXAMS
-                                </div>
-
-                            </div>
-
-                            <div className={toCss(s_footerArea)}>
-                                <Button 
-                                    appearance='primary' 
-                                    className={toCss(s_saveBtn)}
-                                    iconBefore='floppy-disk'
-                                    isLoading={state.saving}
-                                    disabled={!state.subject?.changed}
-                                    onClick={saveHandler}
-                                >
-                                    Save
-                                </Button>
-                            </div>
-                            
                         </div>
 
-                        <Dialog
-                            isShown={wantDelete}
-                            isConfirmLoading={deleting}
-                            title={`Delete subject ${state.subject?.name}`}
-                            confirmLabel='Confirm'
-                            onCancel={close => {
-                                setWantDelete(false);
-                                close();
-                            }}
-                            onConfirm={close => {
-                                if (!state.subject) {
-                                    dispatch(setError('Unexpected error.'));
-                                } else {
-                                    setDeleting(true);
-                                    deleteSubject(state.subject.id)
-                                        .then(() => {
-                                            close();
-                                            history.push('/settings');
-                                        })
-                                        .catch(error => {
-                                            dispatch(setError(error.message));
-                                            setDeleting(false);
-                                            setWantDelete(false);
-                                        })
-                                }
-                            }}
-                            onCloseComplete={() => { //closed in another way
-                                setWantDelete(false);
-                            }}
-                        >
-                            Are you sure you want to delete this subject including all of the exams, events and tasks?
-                        </Dialog>
-
-                    </div>
-
-                );
-            }}
-        </Transition>
+                    );
+                }}
+            </Transition>
+            <Prompt
+                when={state.subject?.changed}
+                message='You have unsaved changes. Proceed?'
+            />
+        </Fragment>
     );
 });
 
