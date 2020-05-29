3dc0df5b46162bb6f113f99204f82f88a54c50e9
Erik Schake <erik.schake@gmail.com>
2020-05-29 23:26:59 +0200

    #24 Edit task start date when interval is once, also edit time always

--- src/components/settings/taskCard/TaskCard.tsx
+++ src/components/settings/taskCard/TaskCard.tsx
@@ -4,7 +4,7 @@ import DateTimePicker from 'react-datepicker';
 import CSS from '../settingsCard/SettingsCard.module.scss';
 import SettingsCard from '../settingsCard/SettingsCard';
 import { TaskModel, IntervalOptions } from '../../../firebase/model';
-import { TaskConfig, getEditedTimestamps, getConfigDataFromTimestamps, getDateFromSeconds, getTimestampFromDate, getFilterForInterval } from './../../../util/timeUtil';
+import { TaskConfig, getEditedTimestamps, getFilterForInterval, sameDay, getDateFromTimestamp, getTimestampFromDate, getDateFromSeconds, getConfigDataFromTimestamps } from './../../../util/timeUtil';
 import { DATETIMEPICKER_DEFAULT_PROPS } from './../../../config/settingsConfig';
 import { toCss } from './../../../util/util';
 import { CustomDateInputUI } from './../customDateInputUI/CustomDateInputUI';
@@ -30,7 +30,15 @@ export default function(props: SubjectDataCardProps<TaskModel>): JSX.Element {
 
     const notifyNotNew = useMemo(() => {
         return () => {
-            toaster.notify('Only the end date can be modified on non-new tasks.', {
+            toaster.notify('Only time and end date can be modified on non-new non-once tasks.', {
+                id: 'unique',
+                duration: 3,
+            });
+        }
+    }, []);
+    const notifyOnce = useMemo(() => {
+        return () => {
+            toaster.notify('Once saved, the date can be changed, but not the interval.', {
                 id: 'unique',
                 duration: 3,
             });
@@ -53,9 +61,9 @@ export default function(props: SubjectDataCardProps<TaskModel>): JSX.Element {
                         selected={getDateFromSeconds(firstDeadline.seconds)}
                         onChange={date => date ? changeHandler({firstDeadline: getTimestampFromDate(date), lastDeadline, interval}) : null}
                         minDate={new Date()}
+                        filterDate={!props.new && interval !== 'once' ? date => sameDay(date, getDateFromTimestamp(firstDeadline)) : undefined}
                         {...DATETIMEPICKER_DEFAULT_PROPS}
-                        disabled={!props.new}
-                        onInputClick={props.new ? undefined : () => notifyNotNew()}
+                        onInputClick={!props.new && interval !== 'once' ? () => notifyNotNew() : undefined}
                     />
                 </div>
 
@@ -84,7 +92,7 @@ export default function(props: SubjectDataCardProps<TaskModel>): JSX.Element {
                         elementType='select-visual'
                         value={interval}
                         onChange={newInterval => !props.new
-                            ? notifyNotNew()
+                            ? interval === 'once' ? notifyOnce() : notifyNotNew()
                             : changeHandler({firstDeadline, lastDeadline, interval: newInterval as string})
                         }
                         options={IntervalOptions}
