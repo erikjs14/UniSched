4f8d965ae397842f9a17b465cda1ebb7cc2cd23f
Erik Schake <erik.schake@gmail.com>
2020-06-07 23:16:48 +0200

    Testimonial section>

---
+++ src/landingPage/components/testimonial/Testimonial.d.ts
@@ -0,0 +1,4 @@
+export interface TestimonialProps {
+    quote: string;
+    from: string;
+}
--- src/landingPage/sections/testimonials/Testimonials.tsx
+++ src/landingPage/sections/testimonials/Testimonials.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import CSS from './Testimonials.module.scss';
 import { TestimonialsProps } from './Testimonials.d';
+import Testimonial from '../../components/testimonial/Testimonial';
+import { toCss } from '../../../util/util';
 const {
     wrapper: s_wrapper,
 } = CSS;
@@ -9,6 +11,11 @@ const {
 export default function(props: TestimonialsProps): JSX.Element {
     
     return (
-        <div>Testimonials</div>
+        <section className={toCss(s_wrapper)}>
+            <Testimonial
+                quote='Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,'                
+                from='Larry Bird'
+            />
+        </section> 
     );
 }
