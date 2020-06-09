12b3f028bd5ea3afee4ee3584edf38f2c0665a71
Erik Schake <erik.schake@gmail.com>
2020-06-09 17:22:31 +0200

    New testimonial

--- src/landingPage/sections/testimonials/Testimonials.tsx
+++ src/landingPage/sections/testimonials/Testimonials.tsx
@@ -16,6 +16,10 @@ export default function(props: TestimonialsProps): JSX.Element {
                 quote='At first, I was suspicious given the colorful design. It seemed a little playful instead of useful. But now I absolutely love it. Seeing the color immediately lets me think of the specific subject. Never before have I been so organized. My life is better now.'
                 from='Very satisfied anonymous user'
             />
+            <Testimonial
+                quote='The perfect app to organize all to-dos, schedules and exams! Makes a lot of fun using it and motivates me to finish my exercises. The usage is very easy and understandable, thus it doesn’t take you more than a few minutes to modify or add related information! I recommend this app to every student who wants to have a structured Uni-life 😁'
+                from='Zahwa Basel'
+            />
         </section> 
     );
 }
